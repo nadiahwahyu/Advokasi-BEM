976d53d53a07ef2cf96913bb1f44cf7e6bc37c0c
okta62 <oktalestari2004@gmail.com>
2025-11-18 17:48:24 +0700

    Menyimpan perubahan sebelum pull

--- pengaduan/form_pengaduan.php
+++ pengaduan/form_pengaduan.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <h2>Form Pengaduan Mahasiswa - Ika</h2>
+    <title>Form Pengaduan Mahasiswa</title>
 </head>
 <body>
-    <h2>Form Pengaduan Mahasiswa (Update Nadiah)</h2>
+    <h2>Form Pengaduan Mahasiswa - Okta (Skema 2)</h2>
+
     <form method="post" action="simpan_pengaduan.php">
+
+        <!-- Okta menambahkan No HP di posisi paling atas -->
+        <label>No HP:</label><br>
+        <input type="text" name="no_hp" required><br><br>
+
         <label>Nama:</label><br>
         <input type="text" name="nama" required><br><br>
 
         <label>NIM:</label><br>
         <input type="text" name="nim" required><br><br>
 
-        <label>Jurusan:</label><br>
-        <input type="text" name="jurusan"><br><br>
-
         <label>Isi Pengaduan:</label><br>
         <textarea name="isi" rows="5" cols="40" required></textarea><br><br>
 
         <button type="submit">Kirim Pengaduan</button>
+
     </form>
 </body>
 </html>
